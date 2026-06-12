4cbbdc2381ac086488541aa7f60bb40161842066
b4Ronin <b4ronin@yahoo.com>
2026-06-11 19:24:32 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fuel-transfer-v17";
+const CACHE_NAME = "fuel-transfer-v99";
 const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
